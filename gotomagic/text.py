9a88db723e5bd6eb9a72c9354313a2b284872728
Robin Garen Aaberg <robin.aaberg@knowit.no>
2018-09-16 00:11:02 +0200

    add ux

---
+++ gotomagic/text.py
@@ -0,0 +1,79 @@
+# -*- code: utf-8 -*-
+"""
+    Text used by GOTO to do UX.
+"""
+
+
+def print_text(text, **kwargs):
+    print(text.format(**kwargs))
+
+
+warning = dict(
+    adding_existing_magicword="""
+
+Ah hoy!
+
+    - You already have a magic word named {magicword},
+    it points to: {uri}
+
+    If you want to override it use:
+        goto update {magicword} {uri}
+    """,
+
+    removing_nonexisting_magicword="""
+Ah hoy!
+
+    - Attempt to remove non-existing shortcut with name {magicword}
+
+    Are you sure it exists in the project you are in now?
+    Type:
+
+        goto list
+
+    to see all shortcuts in this project.
+    """,
+
+    magicword_does_not_exist="""
+Ah hoy!
+
+    - The Magic word {magicword} does not exist.
+
+    Are you sure it exists in the project you are in now?
+    Type:
+
+        goto list
+
+    to see all shortcuts in this project.
+    """,
+
+    no_magicword_named_code="""
+Ah hoy!
+    - No magicword named code.
+    Create it with
+        goto add code <path to folder>
+    in order to be able to run goto subl.
+    """,
+
+    missing_uri="""
+Ah hoy!
+    - Remember, a shortcut has a name and a target uri (or path).
+
+    Try again by adding another argument with where the shortcut
+    should go to:
+
+                goto add {magicword} <uri>
+    """,
+
+    missing_magicword_and_uri="""
+Ah hoy!
+    - Remember, a shortcut has a name and a target uri (or path).
+
+    Try again by adding both a name and a uri/path to where the shortcut
+    should go to:
+
+                goto add <magicword> <uri>
+
+    example:
+        goto add website http://example.org
+    """
+)
--- the_real_goto.py
+++ the_real_goto.py
@@ -3,49 +3,108 @@
 'Goto - the magic project that takes you where you need to be, now.'
 import sys
 from gotomagic.handlers import *
-from gotomagic.magic import load_magic, save_magic
+from gotomagic.magic import GotoMagic
+import gotomagic.text as text
+from gotomagic.text import print_text
 
 
-def list_shortcuts(magic, args):
-    if "-v" in args:
-        for k, v in magic.items():
-            print("%16s --> %s" % (k, v))
-    else:
-        for k in magic.keys():
-            print(k)
+def usage():
+    return """
+    Goto - the magic traveler, how may I help you?
+
+    Wondering how to change project?
+     - consult my brother in command by typing:
+        project help
+
+    The basics
+        - Adding a shortcut:        goto add <magicword> <url or path>
+        - Updating a shortcut:      goto update <magicword> <url or path>
+        - Removing a shortcut:      goto rm <magicword>
+
+        - Show url of shortcut:     goto show <magicword>
+
+        - Listing all shortcuts:    goto list
+        - With the urls printed:    goto list -v
+
+    Working with folders and files:
+        - cd in terminal:           goto cd <magicword>
+        - open folder in Finder:    goto -f <magicword>
+
+    code - A specially magic magic word:
+        If you add a shortcut
+        to a folder,and name the
+        shortcut "code";            goto add code <path to folder with code>
+        - you may then do this:     goto subl
+        and it opens
+        Sublime Text in your folder.
+
+    """
 
 
 if __name__ == "__main__":
     jfile = sys.argv[1]
-    magic = load_magic(jfile)
+    magic = GotoMagic(jfile)
+
+    if len(sys.argv) == 2:
+        print(usage())
+        exit(0)
 
     if len(sys.argv) > 2:
+        if sys.argv[2] in ['help', '-h', '/?', '--help']:
+            print(usage())
+            exit(0)
+
         if sys.argv[2] == 'add':
-            magic[sys.argv[3]] = sys.argv[4]
-            save_magic(jfile, magic)
-            print('Added magic word %s' % sys.argv[3])
+            try:
+                magic.add_shortcut(sys.argv[3], sys.argv[4])
+                magic.save()
+                print('Added magic word %s' % sys.argv[3])
+                exit(0)
+            except IndexError:
+                if len(sys.argv) > 3:
+                    print_text(
+                        text.warning["missing_uri"],
+                        magicword=sys.argv[3]
+                    )
+                else:
+                    print_text(
+                        text.warning["missing_magicword_and_uri"]
+                    )
+                exit(1)
+
+        if sys.argv[2] == 'update':
+            magic.update_shortcut(sys.argv[3], sys.argv[4])
+            magic.save()
+            print('Updated magic word %s' % sys.argv[3])
             exit(0)
 
         if sys.argv[2] == 'rm':
-            magic.pop(sys.argv[3])
-            save_magic(jfile, magic)
-            print('Removed magic word %s' % sys.argv[3])
-            exit(0)
+            try:
+                magic.remove_shortcut(sys.argv[3])
+                magic.save()
+                print('Removed magic word %s' % sys.argv[3])
+                exit(0)
+            except Exception:
+                print('Failed to remove magic word %s' % sys.argv[3])
+                exit(1)
 
         if sys.argv[2] == 'show':
-            print(magic[sys.argv[3]])
+            magic.show_shortcut(sys.argv[3])
             exit(0)
 
         if sys.argv[2] == 'copy':
-            copy_to_clipboard(str(magic[sys.argv[3]]))
+            copy_to_clipboard(str(magic.get_uri(sys.argv[3])))
             exit(0)
 
         if sys.argv[2] == 'list':
-            list_shortcuts(magic, sys.argv)
+            magic.list_shortcuts(verbose=('-v' in sys.argv))
             exit(0)
 
         if sys.argv[2] == 'subl':
-            open_sublime(magic['code'])
+            try:
+                open_sublime(magic['code'])
+            except KeyError:
+                print(text.warning["no_magicword_named_code"])
             exit(0)
 
         if '-f' in sys.argv:
