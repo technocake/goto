a08c1f5b64325f528ef849b02b4d2e429ca05e62
Robin Garen Aaberg <robin.aaberg@knowit.no>
2018-09-16 00:49:56 +0200

    prompt user whether or not to append startupscript to .bash_profile

--- install_goto.sh
+++ install_goto.sh
@@ -8,7 +8,29 @@ touch ${HOME}/.goto/active-project
 ln -s /usr/local/opt/goto/bin/* /usr/local/bin/
 
 # add init_script to.bash_profile:
-echo "source start_goto" >> ${HOME}/.bash_profile
+
+echo "Goto needs to run a startup script for it to work. "
+echo "This can be automatically added now to your .bash_profile file"
+echo "If you want to do this manually add the line 'source start_goto'"
+
+if [ -f "${HOME}/.bash_profile" ]; then
+    read -p "Want to automatically add the goto startup script to your .bash_profile? " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "source start_goto" >> ${HOME}/.bash_profile
+    fi
+else
+    echo "~/.bash_profile does not exist"
+    echo 
+    echo "To make goto function properly, add this line to your bash config file: "
+    echo
+    echo "         source start_goto"
+    echo
+    echo "into one of these (.bashrc | .profile | .bash_profile)"
+
+fi
+
 
 # make goto ready -- now.
 source start_goto
