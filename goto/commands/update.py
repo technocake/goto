ccd8993b474f1b8dcac23a5e481c64ecd5a96d30
Jonas Johan solsvik <jonasjso@protonmail.com>
2019-07-07 01:38:51 +0200

    add commands/update.py

---
+++ goto/commands/update.py
@@ -0,0 +1,10 @@
+import sys
+
+def run(magic):
+    magic.update_shortcut(sys.argv[3], sys.argv[4])
+    magic.save()
+    print('Updated magic word %s' % sys.argv[3])
+    return 0
+
+if __name__ == "__main__":
+    run()
