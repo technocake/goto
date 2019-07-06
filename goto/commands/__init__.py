01ad77e630b2b9e764fe73897ea71d112d018d87
Jonas Johan solsvik <jonasjso@protonmail.com>
2019-07-07 01:01:36 +0200

    add commands/usage.py

---
+++ goto/commands/__init__.py
@@ -0,0 +1,1 @@
+from usage import run as usage
--- goto/the_real_goto.py
+++ goto/the_real_goto.py
@@ -12,6 +12,9 @@ from .gotomagic.magic import GotoMagic, is_file
 from .gotomagic import text
 from .gotomagic.text import print_text
 
+from .commands import \
+    usage \
+
 
 # make sure we print in utf-8
 try:
@@ -20,47 +23,12 @@ try:
 except:
     pass  # TODO: implement utf-8 encoding of py2.7
 
-
-def usage():
-    return """
-    Goto - the magic traveler, how may I help you?
-
-    Wondering how to change project?
-        project help                  Consult my brother in command
-
-    The basics
-        goto <magicword>                        Go to shortcut
-        goto add    <magicword> <url or path>   Add shortcut
-        goto update <magicword> <url or path>   Update shortcut
-        goto rm     <magicword>                 Remove shortcut
-        goto show   <magicword>                 Show url of shortcut
-        goto list                               List all shortcuts
-        goto list -v                            With the urls printed
-
-    Working with folders and files
-        goto <magicword>              Goto will cd to a folder shortcut by default.
-        goto cd   <magicword>         cd in terminal
-        goto open <magicword>         Open in finder/file explorer
-        goto -o   <magicword>                                    
-
-    If you add a shortcut to a folder, and name it "code"...
-        goto add code <path to folder with code>
-
-    ...this command will open folder with Sublime Text
-        goto subl
-
-    Other editors supported:
-    Visual Studio Code: goto vscode | IntelliJ: goto idea    
-    """  # noqa
-
-
 def main():
     jfile = sys.argv[1]
     magic = GotoMagic(jfile)
 
     if len(sys.argv) == 2:
-        print(usage())
-        exit(0)
+        return usage()
 
     if len(sys.argv) > 2:
         if sys.argv[2] in ['help', '-h', '/?', '--help']:
