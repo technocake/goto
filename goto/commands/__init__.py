8f65a55afc46a53d2d205850b956f9c06b59fe41
Jonas Johan solsvik <jonasjso@protonmail.com>
2019-07-07 01:30:53 +0200

    add commands/add.py

--- goto/commands/__init__.py
+++ goto/commands/__init__.py
@@ -1,1 +1,3 @@
+from add import run as add
 from usage import run as usage
+from update import run as update
---
+++ goto/commands/add.py
@@ -0,0 +1,26 @@
+import sys
+from ..gotomagic.text import print_text
+from ..gotomagic import text
+
+def run(magic):
+    try:
+        magic.add_shortcut(sys.argv[3], sys.argv[4])
+        magic.save()
+        print('Added magic word %s' % sys.argv[3])
+        return 0
+    except IndexError:
+        if len(sys.argv) > 3:
+            print_text(
+                text.warning["missing_uri"],
+                magicword=sys.argv[3]
+            )
+            return 1
+        else:
+            print_text(
+                text.warning["missing_magicword_and_uri"]
+            )
+        return 1
+
+
+if __name__ == "__main__":
+    run()
